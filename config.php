24ba831a6b931eb3da69106b6b216ba595afefcd
hongcheng.xu <hongcheng.xu@rayjoy.com>
2015-12-16 17:44:52 +0800

    aa

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ define('DB_HOST', SAE_MYSQL_HOST_M);
 define('DB_USER', SAE_MYSQL_USER);
 define('DB_PWD', SAE_MYSQL_PASS);
 define('DB_PORT', SAE_MYSQL_PORT);
-define('DB_NAME', 'cnblogs_cms');
+define('DB_NAME', 'app_ykcnblogs');
