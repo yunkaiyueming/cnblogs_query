bb2f5a8d54cf85755eabf52e7e38893b5b40e215
hongcheng.xu <hongcheng.xu@rayjoy.com>
2015-12-21 15:02:09 +0800

    修改数据库配置

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PWD', '123456');
-define('DB_PORT', 3306);
-define('DB_NAME', 'cnblogs_cms');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PWD', '123456');
+// define('DB_PORT', 3306);
+// define('DB_NAME', 'cnblogs_cms');
 
-// define('DB_HOST', SAE_MYSQL_HOST_M);
-// define('DB_USER', SAE_MYSQL_USER);
-// define('DB_PWD', SAE_MYSQL_PASS);
-// define('DB_PORT', SAE_MYSQL_PORT);
-// define('DB_NAME', 'app_ykcnblogs');
+define('DB_HOST', SAE_MYSQL_HOST_M);
+define('DB_USER', SAE_MYSQL_USER);
+define('DB_PWD', SAE_MYSQL_PASS);
+define('DB_PORT', SAE_MYSQL_PORT);
+define('DB_NAME', 'app_ykcnblogs');
