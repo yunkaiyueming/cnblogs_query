5b653409a1fa7e61d73d49d2f75f28d1f3a99a8a
yunyun.cai <1835373375@qq.com>
2016-01-09 15:36:47 +0800

    thinkphp代码修改

--- record_cn_thinkphp.php
+++ record_cn_thinkphp.php
@@ -9,7 +9,7 @@ $con = mysql_connect(DB_HOST . ":" . DB_PORT, DB_USER, DB_PWD);
 mysql_select_db(DB_NAME);
 
 $base_url = "http://www.thinkphp.cn/extend";
-$fetch_url_types = array('engine', 'function', 'library',);
+$fetch_url_types = array('engine', 'function', 'library','driver','action','model','behavior','mode','example','others');
 
 $reg = array(
 	'title' => array('a', 'text'),
@@ -19,22 +19,19 @@ $rang = ".article-title";
 
 foreach($fetch_url_types as $url_type){
 	$real_fetch_url = $base_url."/".$url_type;
-	
 	//每类抓取2页
-	for($i=1; $i<3;$i++){
+	for($i=1; $i<6;$i++){
 		$url = $real_fetch_url."/p/".$i.".html";
 		$hj = QueryList::Query($url, $reg, $rang, 'UTF-8');
 		$cn_thinkphp_infos = $hj->data;
-		
 		$arr_exit = get_all_url();
 		//数据入库
 		foreach ($cn_thinkphp_infos as $cn_thinkphp_info) {
 			//从数据库中查处所有已经有的数据，进行对比，如果有，则不插入
-			if(!in_array($cn_thinkphp_info['title'], $arr_exit)){
+			if(!in_array('http://www.thinkphp.cn'.$cn_thinkphp_info['title_url'], $arr_exit)){
 				$cn_thinkphp_info['title_url'] = 'http://www.thinkphp.cn'.$cn_thinkphp_info['title_url'];
 				$values = implode("','", $cn_thinkphp_info);
 				$sql = "insert into cn_thinkphp_tuijian(title,title_url) values('$values')";
-				
 				mysql_query($sql);
 				echo 'success';
 			}
